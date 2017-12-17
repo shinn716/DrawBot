8312e0fa1a1f57946146257422b907a045837ddf
Tung <shinn716@livemail.tw>
2017-12-17 15:19:48 +0800

    Add files via upload
    Arduino

---
+++ drawBotv5_ado_final/drawBotv5_ado.ino
@@ -0,0 +1,448 @@
+// Sonic Signer
+// cc - by Shinn 2017
+
+// Plotclock
+// cc - by Johannes Heberlein 2014
+// 1.03  Fixed the length bug at the servo2 angle calculation, other fixups
+
+#include <Servo.h>
+
+// delete or mark the next line as comment if you don't need these
+//#define CALIBRATION      // enable calibration mode
+
+#define WISHY 3 // Offset of the Y coordinats of the plate-wisher   3
+
+// When in calibration mode, adjust the following factors until the servos move exactly 90 degrees
+#define SERVOFAKTORLEFT 580
+#define SERVOFAKTORRIGHT 580
+
+// Zero-position of left and right servo
+// When in calibration mode, adjust the NULL-values so that the servo arms are at all times parallel
+// either to the X or Y axis
+#define SERVOLEFTNULL  1950
+#define SERVORIGHTNULL 815
+
+#define SERVOPINLIFT  9
+#define SERVOPINLEFT  10    //11
+#define SERVOPINRIGHT 11    //10
+
+#define ZOFF -200       //  p1-62   p2-  -200
+// lift positions of lifting servo
+#define LIFT0 1100+ZOFF  // on drawing surface     1110+ZOFF / 1155+ZOFF
+#define LIFT1 995+ZOFF  // between numbers        995+ZOFF
+#define LIFT2 510+ZOFF  // going towards sweeper  735+ZOFF
+
+// speed of liftimg arm, higher is slower
+#define LIFTSPEED 1000          //1000
+
+// length of arms
+#define L1 35
+#define L2 55.1
+#define L3 13.2
+#define L4 45
+
+// origin points of left and right servo
+#define O1X 24//22
+#define O1Y -25
+#define O2X 49//47
+#define O2Y -25
+
+#include <Wire.h>
+
+int servoLift = 855;   //起始lift 數值950
+
+Servo servo1;  //
+Servo servo2;  //
+Servo servo3;  //
+
+volatile double lastX = 75;
+volatile double lastY = 47.5;
+int last_min = 0;
+
+
+//----shinn
+String inString = "";
+float offpx = 74.1;
+float offpy = 28.2;
+
+void setup()
+{
+  Serial.begin(9600);
+  if (!servo1.attached()) servo1.attach(SERVOPINLIFT);
+  if (!servo2.attached()) servo2.attach(SERVOPINLEFT);
+  if (!servo3.attached()) servo3.attach(SERVOPINRIGHT);
+  lift(1);
+}
+
+void loop()
+{
+
+  //  delay(10);
+
+#ifdef CALIBRATION
+
+  if (!servo1.attached()) servo1.attach(SERVOPINLIFT);
+  if (!servo2.attached()) servo2.attach(SERVOPINLEFT);
+  if (!servo3.attached()) servo3.attach(SERVOPINRIGHT);
+
+  // Servohorns will have 90° between movements, parallel to x and y axis
+  drawTo(-3, 29.2);
+  delay(2000);
+  drawTo(74.1, 28);
+  delay(2000);
+
+#else
+
+  if (!servo1.attached()) servo1.attach(SERVOPINLIFT);
+  if (!servo2.attached()) servo2.attach(SERVOPINLEFT);
+  if (!servo3.attached()) servo3.attach(SERVOPINRIGHT);
+
+  if (Serial.available() > 0)
+  {
+    char inChar = Serial.read();
+    String str_x = " ", str_y = " ", str_d = " ";
+
+    if (isDigit(inChar))
+    {
+      inString += (char)inChar;
+    }
+
+    if (inChar == '\n')
+    {
+      Serial.println("length " + inString.length() );
+      str_x = inString.substring(0, 3);
+      str_y = inString.substring(3, 6);
+      str_d = inString.substring(6, 7);
+
+
+      float f_str_x = 0.1 * str_x.toFloat();
+      float f_str_y = 0.1 * str_y.toFloat();
+      int i_str_d = str_d.toInt();
+
+
+      if (abs(f_str_x - offpx) > 1) {
+        offpx = f_str_x;
+      }
+      if (abs(f_str_y - offpy) > 1) {
+        offpy = f_str_y;
+      }
+
+
+      drawTo(offpx, offpy);
+
+      if (i_str_d == 1) lift(0);
+      if (i_str_d == 0) lift(1);
+
+      Serial.print("String slice:");
+      Serial.print(f_str_x );
+      Serial.print(" " );
+      Serial.print(f_str_y );
+      Serial.print(" " );
+      Serial.println(i_str_d );
+
+      inString = "";
+    }
+
+    //    }
+
+  }
+  else
+  {
+    servo1.detach();
+    servo2.detach();
+    servo3.detach();
+  }
+
+#endif
+
+}
+
+// Writing numeral with bx by being the bottom left originpoint. Scale 1 equals a 20 mm high font.
+// The structure follows this principle: move to first startpoint of the numeral, lift down, draw numeral, lift up
+void number(float bx, float by, int num, float scale) {
+
+  switch (num) {
+
+    case 0:
+      drawTo(bx + 12 * scale, by + 6 * scale);
+      lift(0);
+      bogenGZS(bx + 7 * scale, by + 10 * scale, 10 * scale, -0.8, 6.7, 0.5);
+      lift(1);
+      break;
+
+    case 1:
+      drawTo(bx + 3 * scale, by + 15 * scale);
+      lift(0);
+      drawTo(bx + 10 * scale, by + 20 * scale);
+      drawTo(bx + 10 * scale, by + 0 * scale);
+      lift(1);
+      break;
+
+    case 2:
+      drawTo(bx + 2 * scale, by + 12 * scale);
+      lift(0);
+      bogenUZS(bx + 8 * scale, by + 14 * scale, 6 * scale, 3, -0.8, 1);
+      drawTo(bx + 1 * scale, by + 0 * scale);
+      drawTo(bx + 12 * scale, by + 0 * scale);
+      lift(1);
+      break;
+
+    case 3:
+      drawTo(bx + 2 * scale, by + 17 * scale);
+      lift(0);
+      bogenUZS(bx + 5 * scale, by + 15 * scale, 5 * scale, 3, -2, 1);
+      bogenUZS(bx + 5 * scale, by + 5 * scale, 5 * scale, 1.57, -3, 1);
+      lift(1);
+      break;
+
+    case 4:
+      drawTo(bx + 10 * scale, by + 0 * scale);
+      lift(0);
+      drawTo(bx + 10 * scale, by + 20 * scale);
+      drawTo(bx + 2 * scale, by + 6 * scale);
+      drawTo(bx + 12 * scale, by + 6 * scale);
+      lift(1);
+      break;
+
+    case 5:
+      drawTo(bx + 2 * scale, by + 5 * scale);
+      lift(0);
+      bogenGZS(bx + 5 * scale, by + 6 * scale, 6 * scale, -2.5, 2, 1);
+      drawTo(bx + 5 * scale, by + 20 * scale);
+      drawTo(bx + 12 * scale, by + 20 * scale);
+      lift(1);
+      break;
+
+    case 6:
+      drawTo(bx + 2 * scale, by + 10 * scale);
+      lift(0);
+      bogenUZS(bx + 7 * scale, by + 6 * scale, 6 * scale, 2, -4.4, 1);
+      drawTo(bx + 11 * scale, by + 20 * scale);
+      lift(1);
+      break;
+
+    case 7:
+      drawTo(bx + 2 * scale, by + 20 * scale);
+      lift(0);
+      drawTo(bx + 12 * scale, by + 20 * scale);
+      drawTo(bx + 2 * scale, by + 0);
+      lift(1);
+      break;
+
+    case 8:
+      drawTo(bx + 5 * scale, by + 10 * scale);
+      lift(0);
+      bogenUZS(bx + 5 * scale, by + 15 * scale, 5 * scale, 4.7, -1.6, 1);
+      bogenGZS(bx + 5 * scale, by + 5 * scale, 5 * scale, -4.7, 2, 1);
+      lift(1);
+      break;
+
+    case 9:
+      drawTo(bx + 9 * scale, by + 11 * scale);
+      lift(0);
+      bogenUZS(bx + 7 * scale, by + 15 * scale, 5 * scale, 4, -0.5, 1);
+      drawTo(bx + 5 * scale, by + 0);
+      lift(1);
+      break;
+
+    case 111:
+
+      lift(0);
+      drawTo(70, 45);
+      drawTo(65 - WISHY, 43);
+
+      drawTo(65 - WISHY, 46);
+      drawTo(5, 49);
+      drawTo(5, 46);
+      drawTo(63 - WISHY, 46);
+      drawTo(63 - WISHY, 42);
+
+      drawTo(5, 42);
+      drawTo(5, 38);
+      drawTo(63 - WISHY, 38);
+      drawTo(63 - WISHY, 34);
+
+      drawTo(5, 34);
+      drawTo(5, 29);
+      drawTo(6, 29);
+      drawTo(65 - WISHY, 26);
+
+      drawTo(5, 26);
+      drawTo(60 - WISHY, 40);
+
+      drawTo(73.2, 44.0);
+      lift(2);
+
+      break;
+
+    case 11:
+      drawTo(bx + 5 * scale, by + 15 * scale);
+      lift(0);
+      bogenGZS(bx + 5 * scale, by + 15 * scale, 0.1 * scale, 1, -1, 1);
+      delay(10);
+      lift(1);
+      drawTo(bx + 5 * scale, by + 5 * scale);
+      lift(0);
+      bogenGZS(bx + 5 * scale, by + 5 * scale, 0.1 * scale, 1, -1, 1);
+      delay(10);
+      lift(1);
+      break;
+
+  }
+}
+
+
+
+void lift(char lift) {
+  switch (lift) {
+    // room to optimize  !
+    case 0: //850
+
+      if (servoLift >= LIFT0) {
+        while (servoLift >= LIFT0)
+        {
+          servoLift--;
+          servo1.writeMicroseconds(servoLift);
+          delayMicroseconds(LIFTSPEED);
+        }
+      }
+      else {
+        while (servoLift <= LIFT0) {
+          servoLift++;
+          servo1.writeMicroseconds(servoLift);
+          delayMicroseconds(LIFTSPEED);
+
+        }
+
+      }
+
+      break;
+
+    case 1: //150
+
+      if (servoLift >= LIFT1) {
+        while (servoLift >= LIFT1) {
+          servoLift--;
+          servo1.writeMicroseconds(servoLift);
+          delayMicroseconds(LIFTSPEED);
+
+        }
+      }
+      else {
+        while (servoLift <= LIFT1) {
+          servoLift++;
+          servo1.writeMicroseconds(servoLift);
+          delayMicroseconds(LIFTSPEED);
+        }
+
+      }
+
+      break;
+
+    case 2:
+
+      if (servoLift >= LIFT2) {
+        while (servoLift >= LIFT2) {
+          servoLift--;
+          servo1.writeMicroseconds(servoLift);
+          delayMicroseconds(LIFTSPEED);
+        }
+      }
+      else {
+        while (servoLift <= LIFT2) {
+          servoLift++;
+          servo1.writeMicroseconds(servoLift);
+          delayMicroseconds(LIFTSPEED);
+        }
+      }
+      break;
+  }
+}
+
+
+void bogenUZS(float bx, float by, float radius, int start, int ende, float sqee) {
+  float inkr = -0.05;
+  float count = 0;
+
+  do {
+    drawTo(sqee * radius * cos(start + count) + bx,
+           radius * sin(start + count) + by);
+    count += inkr;
+  }
+  while ((start + count) > ende);
+
+}
+
+void bogenGZS(float bx, float by, float radius, int start, int ende, float sqee) {
+  float inkr = 0.05;
+  float count = 0;
+
+  do {
+    drawTo(sqee * radius * cos(start + count) + bx,
+           radius * sin(start + count) + by);
+    count += inkr;
+  }
+  while ((start + count) <= ende);
+}
+
+
+void drawTo(double pX, double pY) {
+  double dx, dy, c;
+  int i;
+
+  // dx dy of new point
+  dx = pX - lastX;
+  dy = pY - lastY;
+  //path lenght in mm, times 4 equals 4 steps per mm
+  c = floor(7 * sqrt(dx * dx + dy * dy));
+
+  if (c < 1) c = 1;
+
+  for (i = 0; i <= c; i++) {
+    // draw line point by point
+    set_XY(lastX + (i * dx / c), lastY + (i * dy / c));
+
+  }
+
+  lastX = pX;
+  lastY = pY;
+}
+
+double return_angle(double a, double b, double c) {
+  // cosine rule for angle between c and a
+  return acos((a * a + c * c - b * b) / (2 * a * c));
+}
+
+void set_XY(double Tx, double Ty)
+{
+  delay(1);
+  double dx, dy, c, a1, a2, Hx, Hy;
+
+  // calculate triangle between pen, servoLeft and arm joint
+  // cartesian dx/dy
+  dx = Tx - O1X;
+  dy = Ty - O1Y;
+
+  // polar lemgth (c) and angle (a1)
+  c = sqrt(dx * dx + dy * dy); //
+  a1 = atan2(dy, dx); //
+  a2 = return_angle(L1, L2, c);
+
+  servo2.writeMicroseconds(floor(((a2 + a1 - M_PI) * SERVOFAKTORLEFT) + SERVOLEFTNULL));
+
+  // calculate joinr arm point for triangle of the right servo arm
+  a2 = return_angle(L2, L1, c);
+  Hx = Tx + L3 * cos((a1 - a2 + 0.621) + M_PI); //36,5°
+  Hy = Ty + L3 * sin((a1 - a2 + 0.621) + M_PI);
+
+  // calculate triangle between pen joint, servoRight and arm joint
+  dx = Hx - O2X;
+  dy = Hy - O2Y;
+
+  c = sqrt(dx * dx + dy * dy);
+  a1 = atan2(dy, dx);
+  a2 = return_angle(L1, L4, c);
+
+  servo3.writeMicroseconds(floor(((a1 - a2) * SERVOFAKTORRIGHT) + SERVORIGHTNULL));
+
+}
